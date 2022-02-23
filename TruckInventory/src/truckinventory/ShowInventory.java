ed6b669efd3949d5e327e2fe184127c05d103c77
Beth <28348518+BethC07@users.noreply.github.com>
2022-02-22 23:22:01 -0500

    Added Inventory.java, Show Inventory.java files
    Added Inventory.java, Show Inventory.java files

---
+++ TruckInventory/src/truckinventory/ShowInventory.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package truckinventory;
+
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+import java.io.*;
+/**
+ *
+ * @author nuria
+ */
+public class ShowInventory extends JFrame {
+    private JTextArea messageArea;
+    private Container contents;
+    private String item, quantity, message;
+    private Scanner file;
+    
+    ArrayList<Inventory> SupplyList = new ArrayList<>( );
+    
+    public ShowInventory(){
+        //
+	// Create Message Area
+	//
+	messageArea = new JTextArea();
+	messageArea.setFont(new Font("Serif", Font.ITALIC, 16));
+	//messageArea.setLineWrap(true);
+	//messageArea.setWrapStyleWord(true);
+	//messageArea.setPreferredSize(new Dimension(400, 200));
+	JScrollPane scrollPane = new JScrollPane(messageArea);    
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);	
+        scrollPane.setPreferredSize(new Dimension(400,200));    
+        //
+	// Create get Content Pane holder
+	//
+	contents = getContentPane( );
+	//
+	// Set Layout
+	//
+	contents.setLayout(new BorderLayout());
+        
+        JPanel p1 = new JPanel();
+	p1.setLayout(new FlowLayout());
+	//p2.add(messageArea);	 
+	p1.add(scrollPane);
+        
+        //
+	// Add Panels to Content Pane
+	//
+        contents.add (p1, BorderLayout.CENTER);
+        
+        setSize(425, 400);
+        //
+        // Show Frame
+        //
+        setVisible(true);
+        
+        try{
+	        
+            file = new Scanner( new File ( "Inventory.txt" ) );   //insert file path       
+	    while ( file.hasNext( ) ) // test for the end of the file
+	        {
+	            // read a line
+	            String stringRead = file.nextLine( );
+	            //
+	            //Notice the StringTokenizer is using the comma as delimiter
+	            //between data values
+	            //
+	
+	             // process the line read
+	            StringTokenizer st = new StringTokenizer( stringRead, "," );
+	            item = st.nextToken( );
+	            quantity = st.nextToken( );
+	            try {
+
+	                Inventory supply = new Inventory(item, quantity);
+
+	                SupplyList.add(supply);
+	            }//End Second Try{}
+	            //
+	            //Catch Numeric DataType Error
+	            //
+	             catch ( NumberFormatException nfe )
+	           {
+	            	 message = "Error in supply record: "
+	                                              + stringRead
+	                                              + "; record ignored" ;	
+	            	 JOptionPane.showMessageDialog( null, message);
+	           }
+	          }//End While
+    		}//End First Try{}
+			catch ( FileNotFoundException fnfe )    	
+	        {
+	        	message = "Unable to find Inventory.txt";
+	        	JOptionPane.showMessageDialog( null, message);
+	        }
+		 
+			finally {
+	       // Release resources associated with books.txt
+	          file.close( ); 	      
+	        }
+        message = "\n";
+        
+        Inventory rInventory = null;
+        for ( int ct=0; ct<SupplyList.size();  ct++) {
+            rInventory = SupplyList.get(ct);
+    	    message = message + "\n"+rInventory.toString();
+    	    		
+    	}
+        
+        messageArea.setText(message);
+        
+    }
+
+            
+}
