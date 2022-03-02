74bf768685cabfe879175733b8c7e54910d2516d
Beth <28348518+BethC07@users.noreply.github.com>
2022-03-02 01:12:30 -0500

    Additional check that value is populated
    Added a check to ensure a value is entered for the restock quantity
    Modified the JOptionPane error messages to show an error icon
    Also modified comments and removed no longer needed code

--- TruckInventory/src/truckinventory/ShowRestockNotification.java
+++ TruckInventory/src/truckinventory/ShowRestockNotification.java
@@ -11,7 +11,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JDialog;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -23,8 +22,9 @@ import javax.swing.JDialog;
  * @author vjsat
  */
 public class ShowRestockNotification extends JFrame{
-    //private ArrayList<Inventory> SupplyList = new ArrayList<>( );
     
+    // function to check if any of the inventory items are low on stock
+    // if they are, show the user the restock notification
     public void checkStock(){
         ArrayList<Inventory> SupplyList = new ArrayList<>();
         SupplyList = ShowInventory.readData();
@@ -38,15 +38,15 @@ public class ShowRestockNotification extends JFrame{
     	}
     }
     
+    // function to show the restock notification to the user
     public void displayNotice(Inventory item){
         JFrame frame;
         JLabel nameL, currentQuantityL, restockThresholdL;
         JButton acceptB, ignoreB;
         Container contents;
         
-    
         frame = new JFrame("Restock Item Notification");
-	// Create text fields, labels and submit button for user to enter the new item data
+	// Create text labels and buttons for user to restock an item or not
 	nameL = new JLabel("This item has a low stock: " + item.getItem());
         currentQuantityL = new JLabel("Quantity on hand: " + item.getQuantity());
         restockThresholdL = new JLabel("Restock Threshold: " + item.getRestockThreshold());
@@ -63,18 +63,17 @@ public class ShowRestockNotification extends JFrame{
 	// Set a grid layout for all the fields
 	contents.setLayout(new GridLayout(4,2));
         
-        // Create panel 1 for the labels and text fields
+        // Create panel 1 for the labels
         JPanel p1 = new JPanel();
 	p1.setLayout(new GridLayout(3,2));	 
 	p1.add(nameL);
         p1.add(currentQuantityL);
         p1.add(restockThresholdL);
         
-        // Create panel 2 for the submit button
+        // Create panel 2 for the buttons
         JPanel p2 = new JPanel();
 	p2.setLayout(new GridLayout(2,0));	 
 	p2.add(acceptB);
-        
         p2.add(ignoreB);
         
 	// Add both panels to Content Pane
@@ -85,10 +84,14 @@ public class ShowRestockNotification extends JFrame{
         frame.pack();
         frame.setSize(400, 400);
         
+        // Action listen for the Restock button
+        // selecting this button will ask the user what value they'd like to restock the item to
+        // once that is submitted, the item's quantity and last restock date/time are updated if it passed the following checks:
+        // - User enters a value in the text field
+        // - Value must be numeric
         acceptB.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                // check if all the fields are populated
                 String itemName, message;
                 int quantity, restockThreshold;
                 
@@ -101,6 +104,7 @@ public class ShowRestockNotification extends JFrame{
                     // ask the user for a restock value
                     String restockValue = (String)JOptionPane.showInputDialog(frame, "Please enter the quantity you would like " + itemName + " restocked to.\nThe default restock value is 10."
                             , "Restock value", JOptionPane.PLAIN_MESSAGE, null, null, 10);
+                    // check if the user entered a value and verify the item is numeric
                     if(restockValue != null && restockValue.length() > 0){
                         int restockValueNumb = Integer.parseInt(restockValue);
                         String ogItemInfo = item.getItem() + "," + item.getQuantity() + "," + item.getRestockThreshold() + "," + item.getRestockDate();
@@ -109,6 +113,10 @@ public class ShowRestockNotification extends JFrame{
                         message = "Item '" + itemName + "' was successfully restocked to " + item.getQuantity();
                         JOptionPane.showMessageDialog(null, message);
                         frame.dispose();
+                    }
+                    else if(restockValue != null && restockValue.length() == 0){
+                        message = "Please enter a quantity to restock the item to";
+                        JOptionPane.showMessageDialog(frame, message, null, JOptionPane.ERROR_MESSAGE);
                     }
                 }
                 catch (Exception e){
@@ -119,12 +127,13 @@ public class ShowRestockNotification extends JFrame{
                     else{
                         message = "Error occured during restock operation";
                     }
-                    JOptionPane.showMessageDialog( null, message);
-                    //frame.dispose();
+                    JOptionPane.showMessageDialog(frame, message, null, JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
         
+        // Action listener for the Ignore button
+        // selecting this button will close the window and not update the item low in stock
         ignoreB.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
